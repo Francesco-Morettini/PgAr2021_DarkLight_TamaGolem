f2a82b188ae5248b6222a4c4da9d97743e17e16f
desikalakar786 <81826196+desikalakar786@users.noreply.github.com>
2021-05-13 20:52:48 +0200

    ciaoone

--- TamaGolem/src/Giocatore.java
+++ TamaGolem/src/Giocatore.java
@@ -4,12 +4,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Giocatore {
+    public static final String ACQUA_PIETRE_DISPONIBILI = "Premi  per 0 per acqua : pietre disponibili -> ";
+    public static final String FUOCO_PIETRE_DISPONIBILI = "Premi  per 1 per fuoco : pietre disponibili ->  ";
+    public static final String ARIA_PIETRE_DISPONIBILI = "Premi  per 2 per aria : pietre disponibili -> ";
+    public static final String TERRA_PIETRE_DISPONIBILI = "Premi  per 3 per terra : pietre disponibili -> ";
+    public static final String ETERE_PIETRE_DISPONIBILI = "Premi  per 4 per etere : pietre disponibili -> ";
+    public static final String SCEGLI_LA_PIETRA_DA_ASSEGNARE_AL_TAMAGOLEM = " scegli la pietra da assegnare al tamagolem:";
+    public static final String ERRORE_INSERIRE_UN_VALORE_CONSONO = "errore, inserire un valore consono";
     private int tamagolemEvocabili;
     private int tamagolemEliminati = 0;
     private Tamagolem tamagolem;
     private int vita;
     private int pietreassegnabili;
-
+    private static final String MSG_ACQUA="";
     public Giocatore(int tamagolemEvocabili, int vita,int pietreassegnabili) {
         this.tamagolemEvocabili = tamagolemEvocabili;
         this.vita=vita;
@@ -29,17 +36,17 @@ public class Giocatore {
             tamagolem= new Tamagolem(vita);
             tamagolemEvocabili--;
             do {
-                System.out.println(" scegli la pietra da assegnare al tamagolem:");
+                System.out.println(SCEGLI_LA_PIETRA_DA_ASSEGNARE_AL_TAMAGOLEM);
                 if (scortapietre[0] > 0)
-                    System.out.println("Premi  per 0 per acqua : pietre disponibili -> "+scortapietre[0]);
+                    System.out.println(ACQUA_PIETRE_DISPONIBILI +scortapietre[0]);
                 if (scortapietre[1] > 0)
-                    System.out.println("Premi  per 1 per fuoco : pietre disponibili ->  "+scortapietre[1]);
+                    System.out.println(FUOCO_PIETRE_DISPONIBILI +scortapietre[1]);
                 if (scortapietre[2] > 0)
-                    System.out.println("Premi  per 2 per aria : pietre disponibili -> "+scortapietre[2]);
+                    System.out.println(ARIA_PIETRE_DISPONIBILI +scortapietre[2]);
                 if (scortapietre[3] > 0)
-                    System.out.println("Premi  per 3 per terra : pietre disponibili -> "+scortapietre[3]);
+                    System.out.println(TERRA_PIETRE_DISPONIBILI +scortapietre[3]);
                 if (scortapietre[4] > 0)
-                    System.out.println("Premi  per 4 per psiche : pietre disponibili -> "+scortapietre[4]);
+                    System.out.println(ETERE_PIETRE_DISPONIBILI +scortapietre[4]);
                 scelta= scanner.leggiIntero("");
                 switch (scelta)
                 {
@@ -69,7 +76,7 @@ public class Giocatore {
                         tamagolem.addPietre(scelta);
                         break;
                     default:
-                        System.out.println("errore, inserire un valore consono");
+                        System.out.println(ERRORE_INSERIRE_UN_VALORE_CONSONO);
                         break;
 
                 }
