ecbe1610321e9a125b02d01384000ca34f4df2ac
Francesco-Morettini <frcmorettini.01@gmail.com>
2021-05-13 11:30:22 +0200

    Aggiunte classi mancanti.

--- TamaGolem/src/Partita.java
+++ TamaGolem/src/Partita.java
@@ -1,98 +1,199 @@
 import java.util.Random;
 
 public class Partita {
-    private int equilibrio[][] = new int[5][5];
-    private int cellesettate=0;
-    private Random r= new Random();
+
+    private final int N = 5;
+    private int equilibrio[][] = new int[N][N];
+
+
+
+
 
     public Partita() {
-        generaEquilibrio();
+        //generaEquilibrio();
     }
 
-    private void generaEquilibrio() {
-        int i ;
 
-    for(i=0;i<5;i++) {
 
-            for (int j =0; j < 5; j++) {
-                if(j>i)
-                getNumeroCasualerighe(i, j);
+
+/*
+    public void generaEquilibrio() {
+
+        Random r = new Random();
+        int cellesettate, cellesettabili, contatorezeri, sommariga;
+
+        for(int i=0; i<3; i++) {
+
+            cellesettate = 0;
+            cellesettabili = 0;
+
+            for (int j=i+1; j<5; j++) {
+                cellesettabili ++;
             }
-            cellesettate=0;
-            // setto la prima colonna
-            //prima era 1 la j
-            for (int j =0 ; j < 5; j++) {
+            cellesettabili--;
 
-                getNumeroCasualecolonna(i);
+            for (int j=0; j<5; j++) {
+
+                if (j>i){
+
+                    getNumeroCasualerighe(i,j,cellesettate,contaZeriRiga(i),cellesettabili,r);
+                    if (equilibrio[i][j]!=0)
+                        cellesettate++;
+
+                }
+
             }
+
+            contatorezeri = contaZeriRiga(i);
+            sommariga = calcolaSommaRiga(i);
+
+
+            for(int j=i+1; j<5; j++) {
+                if (contatorezeri>=1){
+                    getNumeroCasualecolonna(j,i,contatorezeri,sommariga,r);
+                    if (equilibrio[i][j] == 0 && i!=j){
+                        contatorezeri--;
+                    }
+                    if (contatorezeri>1){
+                        sommariga -= equilibrio[j][i];
+                    }
+                }
+            }
+
+        }
+
     }
+*/
 
-        //stampa array
-        for (i = 0; i < 5; i++) {
+/*
+    public void stampaEquilibrio(){
+
+        for (int i=0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 System.out.print(equilibrio[i][j]);
 
             }
             System.out.println("");
         }
-    }
-
-    private void getNumeroCasualerighe(int riga,int colonna) {
-        int sommariga = 0,sommacolonna=0;
-        for (int j = 0; j < 5; j++) {
-            sommacolonna += equilibrio[j][riga];
-        }
-        for (int j = 0; j < 5; j++)
-            sommariga += equilibrio[riga][j];
-
-            if (sommariga <10 && (cellesettate < 3 - riga || cellesettate==0) && sommacolonna<10 )//cellesettate (cellesettate < 3 - riga || cellesettate=0)
-            {
-
-                if(10-Math.max(sommariga,sommacolonna)>0) {
-                    cellesettate++;
-
-                    equilibrio[riga][colonna] = r.nextInt(10 - Math.max(sommariga, sommacolonna));
-                }
-            }
 
     }
+*/
 
-    private void getNumeroCasualecolonna(int colonna) {
-        int sommariga = 0, sommacolonna = 0, contatorezeri = -1;
-        for (int j = 0; j < 5; j++) {
-            sommariga += equilibrio[colonna][j];
-        }
-        for (int j = 0; j < 5; j++) {
-            sommacolonna += equilibrio[j][colonna];
-        }
-        for (int j = 0; j < 5; j++) {
-            if (equilibrio[colonna][j] == 0 ) {
-                contatorezeri++;
-            }
-        }
-            if (sommacolonna < 10  ) {
-                for (int j = 1; j < 5; j++) {
-                    if (equilibrio[colonna][j] == 0 && j!=colonna) {
-                        switch (contatorezeri)
-                        {
-                            case 1:
-                                equilibrio[j][colonna]=sommariga;
-                                break;
-                            default:
-                                if(sommariga-sommacolonna>0) {
-                                    equilibrio[j][colonna] = r.nextInt(sommariga - sommacolonna) + 1;
-                                    contatorezeri--;
-                                    sommariga -= equilibrio[j][colonna];
-                                }
-                                break;
 
-                        }
+/*
+    public int contaZeriRiga(int riga){
+        int contatorezeri=0;
+
+        for (int k=riga+1; k<5; k++) {
+            if (equilibrio[riga][k]==0)
+                contatorezeri ++;
+        }
+
+        return contatorezeri;
+
+    }
+*/
+
+
+/*
+    public int calcolaSommaRiga(int riga){
+        int sommariga = 0;
+        for (int i=0; i<5; i++) {
+            sommariga += equilibrio[riga][i];
+        }
+        return sommariga;
+    }
+*/
+
+
+/*
+    public int calcolaSommaColonna(int colonna){
+        int sommacolonna = 0;
+        for (int i=0; i<5; i++) {
+            sommacolonna += equilibrio[i][colonna];
+        }
+        return sommacolonna;
+    }
+*/
+
+
+/*
+    public void getNumeroCasualerighe(int riga, int colonna, int cellesettate,int contatorezeri, int cellesettabili, Random r) {
+
+        int sommariga, sommacolonna, numeroCasuale;
+
+        sommacolonna = calcolaSommaColonna(riga);
+        sommariga = calcolaSommaRiga(riga);
+
+        if (sommariga<10 && sommacolonna<10){
+
+            if(cellesettate<cellesettabili){
+                    if(10-Math.max(sommariga,sommacolonna)>0) {
+                        //genera numero casuale maggiore di 0
+                        numeroCasuale = r.nextInt(10 - Math.max(sommariga, sommacolonna));
+                        //n-1
+                        if (colonna == 4 && contatorezeri>cellesettabili) {
+                            switch (numeroCasuale) {
+                                case 0:
+                                    equilibrio[riga][colonna] = 1;
+                                    break;
+
+                                default:
+                                    if (numeroCasuale < sommacolonna)
+                                        equilibrio[riga][colonna] = sommacolonna;
+                                    else
+                                        equilibrio[riga][colonna] = numeroCasuale;
+                                    break;
+
+                            }
+                        }else{
+                                if (numeroCasuale < sommacolonna)
+                                    equilibrio[riga][colonna] = sommacolonna;
+                                else
+                                    equilibrio[riga][colonna] = numeroCasuale;
+                            }
 
                     }
 
+            }
+
+       }
+
+    }
+*/
+
+
+
+/*
+    public void getNumeroCasualecolonna(int riga, int colonna, int contazeri, int sommariga, Random r) {
+
+        int sommacolonna;
+
+        sommacolonna = calcolaSommaColonna(colonna);
+
+        if (sommacolonna < 10) {
+
+            if (equilibrio[colonna][riga] == 0 && colonna!=riga) {
+
+                switch (contazeri){
+                    case 1:
+                        equilibrio[riga][colonna]=sommariga - sommacolonna;
+                        break;
+
+                    default:
+                        if(sommariga-sommacolonna>0) {
+                            equilibrio[riga][colonna] = r.nextInt(sommariga - sommacolonna) - contazeri;
+                        }
+                        break;
+
                 }
+
             }
 
         }
+
     }
+*/
+
+}
 
