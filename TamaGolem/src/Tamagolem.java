04b9ffd2fd56119fac061b0e401cfe3535e076de
Matteo <m.mora018@studenti.unibs.it>
2021-05-13 12:11:10 +0200

    classe Tamagolem

--- TamaGolem/src/Tamagolem.java
+++ TamaGolem/src/Tamagolem.java
@@ -1,5 +1,21 @@
-public class Tamagolem {
+import java.util.ArrayList;
 
+public class Tamagolem {
+    private ArrayList<String> pietreSelezionate;
+    private int vitaDisponibile;
+
+    public Tamagolem(int vita) {
+        pietreSelezionate = new ArrayList<String>();
+        this.vitaDisponibile = vita; //set vita da parametro
+    }
+
+    public int getVitaDisponibile() {
+        return vitaDisponibile;
+    }
+
+    public void setVitaDisponibile(int danno) {
+        vitaDisponibile = vitaDisponibile - danno;
+    }
 
 
 }
